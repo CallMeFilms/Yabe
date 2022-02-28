57d252ceb9ba05d483af8ac9c79466a895b92208
geoday03 <geoday.ai.si@gmail.com>
2022-02-27 20:09:06 -0800

    finished register slash commands script

--- commands/buildSlashCommands.js
+++ commands/buildSlashCommands.js
@@ -1,7 +1,10 @@
-// const { SlashCommandBuilder } = require('@discordjs/builders');
-// const { REST } = require('@discordjs/rest');
-// const { Routes } = require('discord-api-types/v9');
-// const { clientId, guildId, token } = require('./config.json');
+// Run this to register all commands as slash commands
+const { SlashCommandBuilder } = require('@discordjs/builders');
+const { REST } = require('@discordjs/rest');
+const { Routes } = require('discord-api-types/v9');
+const token = process.env.BOT_TOKEN
+const clientId = process.env.CLIENT_ID
+const guildId = "348999247749971968" // Testing server ID
 
 const commands = []
 
@@ -13,24 +16,18 @@ require('fs')
 			if (commandCategory != "buildSlashCommands.js") {
 				require('fs').readdir(`./${commandCategory}`, (err, commandArray) => {
 					commandArray.forEach(command => {
-						console.log(command.slice(0, -3))
+						commands.push(command.slice(0, -3))
 					})
 				})
 			}
 		})
 	});
 
-// commands.map(command => command.toJSON());
+commands.map(command => command.toJSON());
 
-// const rest = new REST({ version: '9' }).setToken(token);
+const rest = new REST({ version: '9' }).setToken(token);
 
-// rest.put(Routes.applicationGuildCommands(clientId, guildId), { body: commands })
-// 	.then(() => console.log('Successfully registered application commands.'))
-// 	.catch(console.error);const testFolder = './tests/';
-// const fs = require('fs');
+rest.put(Routes.applicationGuildCommands(clientId, guildId), { body: commands })
+	.then(() => console.log('Successfully registered application commands.'))
+	.catch(console.error);
 
-// fs.readdir(testFolder, (err, files) => {
-//   files.forEach(file => {
-//     console.log(file);
-//   });
-// });
